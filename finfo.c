da3436d1288b243858412ca5b939b8c4bb560819
Riccardo Mancini <riccardo.mancini01@gmail.com>
2025-09-23 00:22:15 +0200

    Fix: close file before exiting

--- finfo.c
+++ finfo.c
@@ -16,19 +16,23 @@ int main(int argc, char *argv[]) {
 
 	FILE *file = fopen(argv[1], "rb");
 	if (file == NULL) {
-		int err = errno;
-		printf("Unable to open file: %s. Error: %d\n", argv[1], err);
+		printf("Unable to open file: %s (%s).\n", argv[1], strerror(errno));
 		return 1;
 	}
 
-	enum {FILE_TYPES_N = 2};
+	enum { FILE_TYPES_N = 2 };
 	bool (*try_type[FILE_TYPES_N])(FILE *) = {try_flac, try_png};
 
 	for (int i = 0; i < FILE_TYPES_N; i++) {
 		// Reset read position in file to make it ready for next try.
 		fseek(file, 0, SEEK_SET); // TODO: check error
-		if (try_type[i](file)) { return 0; }
+
+		if (try_type[i](file)) {
+			fclose(file);
+			return 0;
+		}
 	}
 
+	fclose(file);
 	return 1;
 }
